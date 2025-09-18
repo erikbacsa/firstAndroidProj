b5c9c58726ba7efc3c5ff402dce6a46ced171cfd
RedRage <eabacsa@gmail.com>
2025-09-17 20:27:52 -0600

    Updated

---
+++ FirstApp/app/src/main/java/com/example/firstapp/FirstFragment.java
@@ -0,0 +1,80 @@
+package com.example.firstapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firstapp.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    TextView showCountTextView;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        // Inflate the layout for this fragment
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+
+        // Get the count text view
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.random_heading);
+
+        return fragmentFirstLayout;
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 NavHostFragment.findNavController(FirstFragment.this)
+                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+             }
+            });
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), R.string.hello_toast, Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
+    }
+
+    private void countMe(View view){
+        // Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        // Convert value to a number and increment it
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
