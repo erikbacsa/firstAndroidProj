7e8b1fa781b6aa1ab0d77273463efc0195621f67
RedRage <eabacsa@gmail.com>
2025-09-17 22:10:51 -0600

    updated

--- FirstApp/build.gradle.kts
+++ FirstApp/build.gradle.kts
@@ -1,4 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Safe Args plugin for Java
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.0")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
 }
